0c0a572202d3b3091a5816073a7f5368ae554e3e
Artem Aisin <artem.aisin@yandex.ru>
2024-11-26 06:11:13 +0300

    feat: added typeorm, entities, init files

--- backend/src/db/db.provider.ts
+++ backend/src/db/db.provider.ts
@@ -1,10 +1,30 @@
 import * as mongoose from 'mongoose';
 import { AppConfig } from '../app.config.provider';
+import { DataSource } from 'typeorm';
+import { Film } from 'src/films/entities/film.entity';
+import { Schedule } from 'src/films/entities/schedule.entity';
 
 export const databaseProvider = {
   provide: 'DB_CONNECT',
-  useFactory: (config: AppConfig) => {
-    return mongoose.connect(config.database.url);
+  useFactory: async (
+    config: AppConfig,
+  ): Promise<typeof mongoose | DataSource> => {
+    if (config.database.driver == 'mongodb') {
+      return await mongoose.connect(config.database.url);
+    } else if (config.database.driver == 'postgres') {
+      const dataSource = new DataSource({
+        type: config.database.driver,
+        host: config.database.host,
+        port: Number(config.database.port),
+        username: config.database.username,
+        password: config.database.password,
+        database: config.database.databasename,
+        entities: [Film, Schedule],
+        synchronize: false,
+      });
+
+      return await dataSource.initialize();
+    }
   },
   inject: ['CONFIG'],
 };
